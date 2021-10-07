e9dd37d31096164531b41f1a4bc3cc8cb37f7324
jovid1242 <jovid1242@gmail.com>
2021-10-07 14:44:40 +0300

    log22

--- controllers/analiticsController.js
+++ controllers/analiticsController.js
@@ -32,7 +32,7 @@ class analiticsController {
       ]; 
       analitics.map((el) => {
         let a = el.createdAt.toLocaleString(); 
-      console.log('colec', typeof (a));
+      console.log('colec', typeof a );
         // arrData[a.toString().split(",")[0].split(".")[1].split("")[1] - 1] +=
         //   +1;
       });
