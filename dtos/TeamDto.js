7e9b314a825757c4a63ebca1d1ea1577b6ebbf7e
jovid1242 <jovid1242@gmail.com>
2021-10-12 17:30:31 +0500

    fix dto team

--- dtos/TeamDto.js
+++ dtos/TeamDto.js
@@ -8,11 +8,6 @@ module.exports = class projectsDto {
     this.id = model.id;
     this.name = model.name;
     this.profession = model.profession;
-    this.image = this.getStorageLink(model.image);
-  }
-
-  getStorageLink(link) {
-    const storageUrl = process.env.STORAGE_URL + "/storage/uploads";
-    return storageUrl + link;
+    this.image = model.image;
   }
 };
